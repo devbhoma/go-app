3d2cf345a47fe728479423c3f850681824015cce
Ram <dev.bhomaram@gmail.com>
2024-04-15 10:17:22 +0530

    pgdb updates

--- internal/database/postgres/database.go
+++ internal/database/postgres/database.go
@@ -7,7 +7,6 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/pkg/errors"
 	"github.com/sqlc-dev/pqtype"
-	"net/url"
 )
 
 type Config struct {
@@ -18,38 +17,7 @@ type Config struct {
 	DisableTLS bool
 }
 
-func Open(cfg Config) (*sql.DB, error) {
-
-	// Define SSL mode.
-	sslMode := "require"
-	if cfg.DisableTLS {
-		sslMode = "disable"
-	}
-
-	// Query parameters.
-	q := make(url.Values)
-	q.Set("sslmode", sslMode)
-	q.Set("timezone", "utc")
-
-	// Construct url.
-	u := url.URL{
-		Scheme:   "postgres",
-		User:     url.UserPassword(cfg.User, cfg.Password),
-		Host:     cfg.Host,
-		Path:     cfg.Name,
-		RawQuery: q.Encode(),
-	}
-
-	return sql.Open("postgres", u.String())
-}
-
-// StatusCheck returns nil if it can successfully talk to the database. It
-// returns a non-nil error otherwise.
 func StatusCheck(ctx context.Context, db *sql.DB) error {
-	// Run a simple query to determine connectivity. The db has a "Ping" method
-	// but it can false-positive when it was previously able to talk to the
-	// database but the database has since gone away. Running this query forces a
-	// round trip to the database.
 	const q = `SELECT true`
 	var tmp bool
 	return db.QueryRowContext(ctx, q).Scan(&tmp)
@@ -74,7 +42,7 @@ func New(driver, host, port, user, pwd, dbname string) (*DB, error) {
 	db.SetMaxOpenConns(400)
 	db.SetMaxIdleConns(100)
 
-	return &DB{SqlDB: db, Mapper: mapper()}, nil
+	return &DB{SqlDB: db}, nil
 }
 
 func ScanRawDataJSON(v interface{}) (pqtype.NullRawMessage, error) {
@@ -110,7 +78,6 @@ func BindRawDataJSON(rawMessage pqtype.NullRawMessage, message interface{}) erro
 type DB struct {
 	SqlDB  *sql.DB
 	unsafe bool
-	Mapper *Mapper
 }
 
 type DBQuery interface {
@@ -124,33 +91,25 @@ type DBQuery interface {
 	GetContext(ctx context.Context, dest interface{}, q string, args ...interface{}) error
 }
 
-// Ping pings the database to check the connection status
 func (db *DB) Ping() error {
 	return db.SqlDB.Ping()
 }
 
-// Close closes the database and prevents new queries from starting.
-// Close then waits for all queries that have started processing on the server
-// to finish.
-//
-// It is rare to Close a DB, as the DB handle is meant to be
-// long-lived and shared between many goroutines.
+
 func (db *DB) Close() error {
 	return db.SqlDB.Close()
 }
 
-// RawSqlDB returns the sql.db implementions
-// useful for anyone to execute queries outside of endpoints
 func (db *DB) RawSqlDB() *sql.DB {
 	return db.SqlDB
 }
 
-// Usually used to retrive only one row
+
 func (db *DB) QueryRow(ctx context.Context, query string, args ...any) *sql.Row {
 	return db.SqlDB.QueryRowContext(ctx, query, args...)
 }
 
-// Used to retrive multiple rows in the table.
+
 func (db *DB) Query(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
 	return db.SqlDB.QueryContext(ctx, query, args...)
 }
@@ -159,49 +118,4 @@ func (db *DB) Exec(ctx context.Context, query string, args ...any) (sql.Result, 
 }
 func (db *DB) BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error) {
 	return db.SqlDB.BeginTx(ctx, opts)
-}
-
-// GetContext using this DB.
-// Any placeholder parameters are replaced with supplied args.
-// An error is returned if the result set is empty.
-func (db *DB) GetContext(ctx context.Context, dest interface{}, q string, args ...interface{}) error {
-	return GetContext(ctx, db.SqlDB, dest, q, args...)
-}
-
-// GetContext does a QueryRow using the provided Queryer, and scans the
-// resulting row to dest.  If dest is scannable, the result must only have one
-// column. Otherwise, StructScan is used.  Get will return sql.ErrNoRows like
-// row.Scan would. Any placeholder parameters are replaced with supplied args.
-// An error is returned if the result set is empty.
-func GetContext(ctx context.Context, db *sql.DB, dest interface{}, query string, args ...interface{}) error {
-	r := QueryRowContext(ctx, db, query, args...)
-	return r.scanAny(dest, false)
-}
-
-// QueryRowContext queries the database and returns an *sqlx.Row.
-// Any placeholder parameters are replaced with supplied args.
-func QueryRowContext(ctx context.Context, db *sql.DB, query string, args ...interface{}) *Row {
-	rows, err := db.QueryContext(ctx, query, args...)
-	return &Row{rows: rows, err: err, unsafe: false, Mapper: mapper()}
-}
-
-// SelectContext using this DB.
-// Any placeholder parameters are replaced with supplied args.
-func (db *DB) SelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
-	return SelectContext(ctx, db.SqlDB, dest, query, args...)
-}
-
-// SelectContext executes a query using the provided Queryer, and StructScans
-// each row into dest, which must be a slice.  If the slice elements are
-// scannable, then the result set must have only one column.  Otherwise,
-// StructScan is used. The *sql.Rows are closed automatically.
-// Any placeholder parameters are replaced with supplied args.
-func SelectContext(ctx context.Context, db *sql.DB, dest interface{}, query string, args ...interface{}) error {
-	rows, err := db.QueryContext(ctx, query, args...)
-	if err != nil {
-		return err
-	}
-	rowsi := &Rows{Rows: rows, unsafe: false, Mapper: mapper()}
-	defer rowsi.Close()
-	return scanAll(rowsi, dest, false)
 }
